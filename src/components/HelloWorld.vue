84ec9ee8a7399ae7211e85a0de3102c05c017284
roughwin <roughwin@qq.com>
2019-06-06 18:24:24 +0800

    base demo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,85 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div>筛选耗时 <span style="color: red">{{timeDuration.toFixed(2)}}</span>ms</div>
+    <div><input type="text" v-model="word" placeholder="输入以过滤：pufa银行"></div>
+    <ol v-if="isInited">
+      <li v-for="(x, index) in filteredList" :key="index">
+        <span
+          v-for="(t, index) of x.text.split('')"
+          :key="index"
+          :style="{
+            color: (x.arr && x.arr.includes(index)) ? 'red' : 'black'
+          }"
+        >{{t}}</span>
+      </li>
+    </ol>
+    <div v-else>loading</div>
   </div>
 </template>
 
+<style scoped>
+  input {
+    height: 2em;
+    width: 200px;
+    margin: 1em 0;
+  }
+  li {
+    list-style: none;
+  }
+</style>
+
+
 <script>
+import Pinyin from 'pinyin-filter';
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  async created() {
+    this.initPinyin();
+    this.getStockList();
+  },
+  data() {
+    return {
+      word: '',
+      list: [],
+      p: undefined,
+      timeDuration: 0,
+    }
+  },
+  methods: {
+    async initPinyin() {
+      const resp = await fetch('/word.json');
+      const dict = await resp.json();
+      this.p = new Pinyin(dict);
+    },
+    async getStockList() {
+      const resp = await fetch('/stock-list.json');
+      this.list = await resp.json();
+    },
+    getFilteredList() {
+      const startTime = window.performance.now();
+      if (!this.word || !this.word.trim()) {
+        this.timeDuration = 0;
+        return this.list.map(x => ({ text: x }))
+      }
+      if (this.p) {
+        const r = this.list.map(x => ({
+          text: x,
+          arr: this.p.test(x, this.word),
+        })).filter(x => x && x.arr && x.arr.length)
+        this.timeDuration = window.performance.now() - startTime;
+        return r;
+      }
+      this.timeDuration = window.performance.now() - startTime;
+      return [];
+    }
+  },
+  computed: {
+    isInited() {
+      return this.p && this.list.length;
+    },
+    filteredList() {
+      return this.getFilteredList();
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
